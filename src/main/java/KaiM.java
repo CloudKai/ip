9ea653bf95cf6e23db9e530358c6ca7caf2d6cb1
CloudKai <kaiming_01@live.com>
2025-01-24 01:19:40 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/KaiM.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class KaiM {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello! I'm KaiM");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
