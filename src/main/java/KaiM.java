f5327525165b36ff303033161a2d7263a4a4a24a
CloudKai <kaiming_01@live.com>
2025-01-24 01:27:33 +0800

    Level 2. Add, List

--- src/main/java/KaiM.java
+++ src/main/java/KaiM.java
@@ -6,19 +6,27 @@ class KaiM {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out, true);
+        
+        int taskCount = 0;
+        String[] tasks = new String[100];   //To store the items
 
         pw.println(" Hello! I'm KaiM");
         pw.println(" What can I do for you?");
 
-        while(true) {
+        while(taskCount < 100) {
             String task = br.readLine();
-            
-            if (!task.equals("bye")) {
-                pw.println(" " + task);
-                
-            } else {
+
+            if(task.equals("bye")) {
                 pw.println(" Bye. Hope to see you again soon!");
                 break;
+            } else if (task.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    pw.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = task;
+                pw.println(" added: " + task);
+                taskCount++;
             }
         }
 
