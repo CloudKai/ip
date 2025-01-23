1fbd3b3880f4a0e41e5a6af4561250e0f85de2e2
CloudKai <kaiming_01@live.com>
2025-01-24 01:23:53 +0800

    Level 1. Echo

--- src/main/java/KaiM.java
+++ src/main/java/KaiM.java
@@ -2,9 +2,26 @@ import java.util.*;
 import java.io.*;
 
 class KaiM {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello! I'm KaiM");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        pw.println(" Hello! I'm KaiM");
+        pw.println(" What can I do for you?");
+
+        while(true) {
+            String task = br.readLine();
+            
+            if (!task.equals("bye")) {
+                pw.println(" " + task);
+                
+            } else {
+                pw.println(" Bye. Hope to see you again soon!");
+                break;
+            }
+        }
+
+        pw.close();
     }
 }
